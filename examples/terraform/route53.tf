2d3028ecc0632d6411094789f14c743daaf74574
Noah Birnel <nbirnel@gmail.com>
2017-07-11 15:15:51 -0700

    add terraform example

---
+++ examples/terraform/route53.tf
@@ -0,0 +1,12 @@
+resource "aws_route53_zone" "example-com" {
+  name   = "example.com."
+}
+
+data "aws_route53_zone" "lighthouse-sec" {
+  name         = "lighthouse.sec"
+  private_zone = true
+}
+
+output "example-com-zone-id" {
+  value = "${aws_route53_zone.zone_id}"
+}
